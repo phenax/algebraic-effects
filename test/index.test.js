288c9098ea881994d222cfac017e51af4f00cd3a
Akshay Nair <phenax5@gmail.com>
2019-01-12 23:34:00 +0530

    Adds test case for createEffect

--- test/index.test.js
+++ test/index.test.js
@@ -1,27 +1,42 @@
 
 import { createEffect } from '../src';
 
-const ApiEffect = createEffect('ApiEffect', {
-  fetch: [],
-});
+describe('createEffect', () => {
+  const ApiEffect = createEffect('ApiEffect', {
+    fetch: ['url', 'request'],
+  });
 
-const apiEffect = ApiEffect.handler({
-  fetch: next => (url, req) => setTimeout(() => next({ url, req, data: 'wow' }), 500),
-});
+  const apiEffect = ApiEffect.handler({
+    fetch: next => (url, req) => setTimeout(() => next({ url, req, data: 'wow' }), 500),
+  });
 
-describe('createEffect example', () => {
-  const action = function *() {
-    const response = yield ApiEffect.fetch('/some-api');
-    yield response.data;
-  };
+  describe('Effect type', () => {
 
-  it('should resolve with the correct value for valid operation', done => {
-    apiEffect(action)
-      .then(data => {
-        expect(data).toBe('wow');
-        done();
-      })
-      .catch(done);
+    it('should have type info', () => {
+      expect(ApiEffect.name).toBe('ApiEffect');
+    });
+
+    it('should have fetch operation', () => {
+      expect(ApiEffect.fetch).toBeInstanceOf(Function);
+      expect(ApiEffect.fetch().name).toBe('fetch');
+      expect(ApiEffect.fetch('/').payload).toEqual(['/']);
+    });
+  });
+  
+  describe('example usage', () => {
+    const action = function *() {
+      const response = yield ApiEffect.fetch('/some-api');
+      yield response.data;
+    };
+  
+    it('should resolve with the correct value for valid operation', done => {
+      apiEffect(action)
+        .then(data => {
+          expect(data).toBe('wow');
+          done();
+        })
+        .catch(done);
+    });
   });
 });
 
