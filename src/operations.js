0569d8c3e0e4cd7557ff7dd02996c8d37569bc58
Akshay Nair <phenax5@gmail.com>
2019-01-17 14:46:21 +0530

    Adds race calling programs

--- src/operations.js
+++ src/operations.js
@@ -6,10 +6,15 @@ export const sleep = Operation('sleep', func(['duration']));
 export const awaitPromise = Operation('awaitPromise', func(['promise a'], 'a'));
 export const call = Operation('call', func(['generator ...a b', '...a'], 'b'));
 export const resolve = Operation('resolve', func(['*']));
+export const race = Operation('race', func(['*']));
+
+// handlePromise :: (...a -> Promise b) -> FlowOperators -> (...a) -> Promise b
+const handlePromise = fn => o => (...args) => fn(o)(...args).then(o.resume).catch(o.throwError);
 
 export default {
   sleep: ({ resume }) => duration => setTimeout(resume, duration),
-  awaitPromise: ({ resume, throwError }) => promise => promise.then(resume).catch(throwError),
+  awaitPromise: handlePromise(() => x => x),
   call: ({ call }) => call,
   resolve: ({ end }) => v => end(v),
+  race: handlePromise(({ call }) => (...programs) => Promise.race(programs.map(p => call(p)))),
 };
