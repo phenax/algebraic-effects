b00f543df086b6958ac5803fed726362897723f8
Akshay Nair <phenax5@gmail.com>
2019-01-19 17:47:09 +0530

    Adds Random effect

---
+++ src/Random.js
@@ -0,0 +1,20 @@
+
+import { createEffect, func } from '.';
+
+// Including min and max i.e. [min, max]
+// getRandomInt :: (Number, Number) -> Number
+const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+
+// Random :: Effect
+const Random = createEffect('Random', {
+  getInt: func(['number', 'number'], 'number'),
+  fromArray: func(['array a'], 'a'),
+});
+
+// Random.effect :: Runner
+Random.effect = Random.handler({
+  getInt: ({ resume }) => (min, max) => resume(getRandomInt(min, max)),
+  fromArray: ({ resume }) => array => resume(array[getRandomInt(0, array.length - 1)]),
+});
+
+export default Random;
---
+++ test/Random.test.js
@@ -0,0 +1,51 @@
+
+import Random from '../src/Random';
+
+const SAMPLE_SET = 20;
+
+describe('Random', () => {
+  
+  describe('getInt', () => {
+    const logFn = jest.fn();
+    const randomizor = function *(a, b) {
+      for(let i = 0; i < SAMPLE_SET; i++)
+        logFn(yield Random.getInt(a, b));
+    };
+
+    afterEach(() => { logFn.mockClear(); });
+  
+    it('should result in a random integer every time', done => {
+      Random.effect(randomizor, 0, 20)
+        .then(() => {
+          logFn.mock.calls.map(x => x[0]).forEach(x => {
+            expect(x).toBeGreaterThanOrEqual(0);
+            expect(x).toBeLessThanOrEqual(20);
+          });
+          done();
+        })
+        .catch(done);
+    });
+  });
+
+  describe('fromArray', () => {
+    const logFn = jest.fn();
+    const randomizor = function *(list) {
+      for(let i = 0; i < SAMPLE_SET; i++)
+        logFn(yield Random.fromArray(list));
+    };
+
+    afterEach(() => { logFn.mockClear(); });
+  
+    it('should result in a random value from the given array', done => {
+      const list = [2342, 112, 'afshkjsz', 'wpw', true, {}, []];
+      Random.effect(randomizor, list)
+        .then(() => {
+          logFn.mock.calls.map(x => x[0]).forEach(x => {
+            expect(list).toContain(x);
+          });
+          done();
+        })
+        .catch(done);
+    });
+  });
+});
