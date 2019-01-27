ca064d53737193cc723db573d33eec21c1b4893b
Akshay Nair <phenax5@gmail.com>
2019-01-27 21:32:52 +0530

    Moves builddocs script to be a multipurpose build/watch/publish tool

--- scripts/docs.js
+++ scripts/docs.js
@@ -1,14 +1,47 @@
-const fsextra = require('fs-extra');
-const watch = require('node-watch');
 const path = require('path');
-const { map, filter, flatten, compose, uniq, prop } = require('ramda');
+const { map, filter, head, compose } = require('ramda');
 
 const webpack = require('webpack');
-const TerserPlugin = require('terser-webpack-plugin');
 
-const { getPackageJson, toPackagePaths, globber, resolveAll, getPackages, errorHandler } = require('./utils');
+const { getPackageJson, toPackagePaths, resolveAll, getPackages, errorHandler } = require('./utils');
 
 process.env.NODE_ENV = 'development';
+
+const [actionName] = process.argv.slice(2);
+
+const toWpConfig = ([ dir, pjson ]) => makeConfig({
+  entry: path.join(dir, 'index.js'),
+  output: {
+    path: path.join(dir, 'public/assets'),
+    filename: 'bundle.js',
+    publicPath: pjson.homepage || '/',
+  },
+});
+
+const onBuildComplete = (err, stats) => {
+  if(err) throw err;
+  console.log(stats.toString({ colors: true }));
+};
+
+const actions = {
+  build: compose(
+    compiler => compiler.run(onBuildComplete),
+    webpack,
+    toWpConfig,
+  ),
+  watch: compose(
+    compiler => compiler.watch(watchOptions, onBuildComplete),
+    webpack,
+    toWpConfig,
+  ),
+  publish: ([ dir ]) => {
+    console.log('Publishing', dir);
+  },
+};
+
+if (!actionName || !actions[actionName]) {
+  throw new Error('You need to specify the action (build, watch, publish)');
+}
 
 const isDoc = dir => {
   try {
@@ -70,20 +103,12 @@ const getDocDirectories = () =>
     .then(filter(isDoc))
     .then(resolveAll);
 
+
+const action = actions[actionName];
+
 getDocDirectories()
   .then(map(dir => [dir, getPackageJson(dir)]))
-  .then(map(([ dir, pjson ]) => makeConfig({
-    entry: path.join(dir, 'index.js'),
-    output: {
-      path: path.join(dir, 'public/assets'),
-      filename: 'bundle.js',
-      publicPath: pjson.homepage || '/',
-    },
-  })))
-  .then(webpack)
-  .then(compiler => compiler.watch(watchOptions, (err, stats) => {
-    if(err) throw err;
-    console.log(stats.toString({ colors: true }));
-  }))
-  .catch(console.error);
+  .then(head)
+  .then(action)
+  .catch(errorHandler);
 
