755fabf637614f6ce0455f1898cf50e55684067c
Akshay Nair <phenax5@gmail.com>
2019-03-21 20:11:29 +0530

    Adds flipCoin method and adds multi mode to Random effect

--- packages/ae-effects/src/Random.js
+++ packages/ae-effects/src/Random.js
@@ -4,9 +4,10 @@ import { compose } from '@algebraic-effects/utils';
 
 // Random :: Effect
 const Random = createEffect('Random', {
-  number: func([], 'number'),
-  getInt: func(['number', 'number'], 'number'),
-  fromArray: func(['array a'], 'a'),
+  number: func(['?times'], 'number', { isMulti: true }),
+  getInt: func(['number', 'number', '?times'], 'number', { isMulti: true }),
+  fromArray: func(['array a', '?times'], 'a', { isMulti: true }),
+  flipCoin: func(['?times'], 'bool', { isMulti: true }),
 });
 
 // Random.seed :: Number -> Runner Number
@@ -21,10 +22,22 @@ Random.seed = seed => {
   // getRandomInt :: (Number, Number) -> Number
   const getRandomInt = (min, max) => Math.floor(random() * (max - min + 1)) + min;
 
+  const pickFromList = list => list[getRandomInt(0, list.length - 1)];
+
+  const flipCoin = () => !!(getRandomInt(0, 100) % 2);
+
+  const wrapMulti = fn => ({ resume }) => (...args) => {
+    const argLength = fn.length;
+    const times = typeof args[argLength] !== 'undefined' ? args[argLength] : 1;
+    times <= 0 && console.log(fn, times);
+    Array(times).fill(null).forEach(() => resume(fn(...args)));
+  };
+
   return Random.handler({
-    number: ({ resume }) => compose(resume, random),
-    getInt: ({ resume }) => compose(resume, getRandomInt),
-    fromArray: ({ resume }) => array => resume(array[getRandomInt(0, array.length - 1)]),
+    number: wrapMulti(random),
+    getInt: wrapMulti(getRandomInt),
+    fromArray: wrapMulti(pickFromList),
+    flipCoin: wrapMulti(flipCoin),
   });
 };
 
--- packages/ae-effects/test/Random.test.js
+++ packages/ae-effects/test/Random.test.js
@@ -133,4 +133,98 @@ describe('Random', () => {
         });
     });
   });
+
+  describe('seeded >> flipCoin', () => {
+    const logFn = jest.fn();
+    const flipCoin = function *(times) {
+      return yield Random.flipCoin(times);
+    };
+
+    afterEach(() => { logFn.mockClear(); });
+  
+    it('should flip a coin', done => {
+      Random.seed(100)
+        .run(flipCoin)
+        .chain(res1 => Random.seed(10).run(flipCoin).map(res2 => [res1, res2]))
+        .fork(done, flipResult => {
+          expect(flipResult).toEqual([ false, true ]);
+          done();
+        });
+    });
+  });
+
+  describe('seeded with multiple continuations', () => {
+    describe('number', () => {
+      function *randomizor(times) {
+        return yield Random.number(times);
+      }
+
+      it('should result in a random number b/w 0,1 every time', done => {
+        Random.seed(10)
+          .runMulti(randomizor, SAMPLE_SET)
+          .fork(done, result => {
+            expect(result).toHaveLength(SAMPLE_SET);
+            result.forEach(x => {
+              expect(x).toBeGreaterThanOrEqual(0);
+              expect(x).toBeLessThanOrEqual(1);
+            });
+            done();
+          });
+      });
+    });
+    
+    describe('getInt', () => {
+      function *randomizor(a, b, times) {
+        return yield Random.getInt(a, b, times);
+      }
+    
+      it('should result in a random integer every time', done => {
+        Random.seed(10)
+          .runMulti(randomizor, 0, 20, SAMPLE_SET)
+          .fork(done, result => {
+            expect(result).toHaveLength(SAMPLE_SET);
+            result.forEach(x => {
+              expect(x).toBeGreaterThanOrEqual(0);
+              expect(x).toBeLessThanOrEqual(20);
+            });
+            done();
+          });
+      });
+    });
+  
+    describe('fromArray', () => {
+      const randomizor = function *(list, times) {
+        return yield Random.fromArray(list, times);
+      };
+    
+      it('should result in a random value from the given array', done => {
+        const list = [2342, 112, 'afshkjsz', 'wpw', true, {}, []];
+        Random.seed(10)
+          .runMulti(randomizor, list, SAMPLE_SET)
+          .fork(done, result => {
+            expect(result).toHaveLength(SAMPLE_SET);
+            result.forEach(x => {
+              expect(list).toContain(x);
+            });
+            done();
+          });
+      });
+    });
+
+    describe('flipCoin', () => {
+      const flipCoin = function *(times) {
+        return yield Random.flipCoin(times);
+      };
+
+      it('should flip a bunch of coins', done => {
+        Random.seed(100)
+          .runMulti(flipCoin, 5)
+          .fork(done, result => {
+            expect(result).toHaveLength(5);
+            expect(result).toEqual([false, false, true, true, false]);
+            done();
+          });
+      });
+    });
+  });
 });
