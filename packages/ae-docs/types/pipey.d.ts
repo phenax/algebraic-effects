1a25471bf9db4cfdfc159f7bfabddda721ceff33
Akshay Nair <phenax5@gmail.com>
2020-02-23 00:54:09 +0530

    Normalized codebase config and typings cleanup

--- packages/ae-docs/types/pipey.d.ts
+++
@@ -1,7 +0,0 @@
-
-declare module 'pipey' {
-  type CurriedMethod = (...args: any) => (o: any) => any;
-  type Methods = { [key: string]: CurriedMethod };
-
-  export function fromClassPrototype(classWithP: any): Methods;
-}
